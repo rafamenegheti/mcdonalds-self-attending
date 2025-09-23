6ce85db69bfc3c136d56e689784dbefd144309bb
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-09-23 15:39:36 -0300

    fix: scroll on product page

--- src/app/[slug]/menu/[productId]/components/ProductHeader.tsx
+++ src/app/[slug]/menu/[productId]/components/ProductHeader.tsx
@@ -1,46 +1,46 @@
-"use client"
-import { Product } from '@prisma/client'
-import { ChevronLeftIcon, ScrollTextIcon } from 'lucide-react'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
 
-import { Button } from '@/components/ui/button'
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
 
 interface ProductHeaderProps {
-  product: Pick<Product, "imageUrl" | "name">;
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
-export default function ProductHeader({ product }: ProductHeaderProps) {
-    const router = useRouter();
-    const handleBackClick = () => {
-        router.back();
-    }
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
   return (
-    <div className='relative w-full h-[300px]'>
-    <Image
-      src={product.imageUrl}
-      alt={product.name}
-      fill
-      className='object-contain'
-    />
+    <div className="relative min-h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
+      >
+        <ChevronLeftIcon />
+      </Button>
 
-    <Button
-      variant={"secondary"}
-      size={"icon"}
-      className="absolute left-4 top-4 z-50 rounded-full"
-      onClick={handleBackClick}
-    >
-      <ChevronLeftIcon />
-    </Button>
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
 
-    <Button
-      variant={"secondary"}
-      size={"icon"}
-      className="absolute right-4 top-4 z-50 rounded-full"
-    >
-      <ScrollTextIcon />
-    </Button>
-  </div>
-  )
-}
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute right-4 top-4 z-50 rounded-full"
+      >
+        <ScrollTextIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductHeader;
